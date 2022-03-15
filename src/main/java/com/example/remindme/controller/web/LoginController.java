3d94babbcba6554b9400ee1eb33a274131c09130
fendi74960 <serpiente61@hotmail.fr>
2022-03-15 14:58:32 +0100

    debut du login ?

---
+++ src/main/java/com/example/remindme/controller/web/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.remindme.controller.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping(value = "/login-form")
+    public String login() {
+        return "login";
+    }
+
+}
