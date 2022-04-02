9ee65c04c79f3dcf700a22a6dd651e719ab22742
fendi74960 <serpiente61@hotmail.fr>
2022-04-02 18:42:14 +0200

    Debut de l'import finis
    on choppe bien le fichier uploaded sous la forme d'un string
    par contre faudrat y nettoyer

--- src/main/java/com/example/remindme/controller/web/ProfilController.java
+++ src/main/java/com/example/remindme/controller/web/ProfilController.java
@@ -1,6 +1,10 @@
 package com.example.remindme.controller.web;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 
+import com.example.remindme.classes.FormWrapper;
+import com.example.remindme.classes.persistence.Event;
 import com.example.remindme.controller.web.service.EventService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,7 +19,14 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 
@@ -31,6 +42,7 @@ public class ProfilController {
     public ProfilController(EventService eventService,UserEntityService userEntityService) {
         this.eventService = eventService;
         this.userEntityService = userEntityService;
+        
     }
 
     
@@ -42,7 +54,7 @@ public class ProfilController {
     }
 
     @PostMapping(value = "/profil/export")
-	public ResponseEntity<InputStreamResource> jsonExport(Model model) throws JsonProcessingException {
+	public ResponseEntity<InputStreamResource> jsonExport() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         byte[] buf = mapper.writeValueAsBytes(eventService.events());
 
@@ -54,6 +66,37 @@ public class ProfilController {
                     MediaType.parseMediaType("application/octet-stream"))
             .body(new InputStreamResource(new ByteArrayInputStream(buf)));
 	}
+
+    // @RequestMapping(value = "/profil/import", method = RequestMethod.POST)
+    // @ResponseBody
+    // public String uploadFile(@RequestParam("file") MultipartFile file) {
+
+    
+    //     return "admin";
+    // }
+    @PostMapping("/profil/import")
+    public ResponseEntity<?> multiUploadFileModel(@ModelAttribute FormWrapper model) throws IOException {
+        try {
+            // Save as you want as per requiremens
+            saveUploadedFile(model.getImage());
+            //formRepo.save(mode.getTitle(), model.getDescription());
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        System.out.println("IM INNNNNNNNNNNNNNNNNNNNNNNNNNNN");
+        System.out.println(new String(model.getImage().getBytes()));
+
+        return new ResponseEntity("Successfully uploaded!", HttpStatus.OK);
+    }
+
+    private void saveUploadedFile(MultipartFile file) throws IOException {
+        /*if (!file.isEmpty()) {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+        }*/
+    }
+
     //https://docs.spring.io/spring-session/reference/guides/boot-redis.html
     //https://docs.spring.io/spring-session/reference/
     //https://stackoverflow.com/questions/13389444/creating-a-java-session
