c9eb4c2080ff0168d5f8d3ea46ad07767fcc8b12
fendi74960 <serpiente61@hotmail.fr>
2022-04-10 17:49:49 +0200

    juste clear les import useless

--- src/test/java/com/example/remindme/LoginControllerTests.java
+++ src/test/java/com/example/remindme/LoginControllerTests.java
@@ -1,34 +1,19 @@
 package com.example.remindme;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import javax.servlet.http.HttpSession;
-
-import com.example.remindme.classes.FormWrapper;
-import com.example.remindme.classes.persistence.UserEntity;
 import com.example.remindme.controller.LoginController;
-import com.example.remindme.controller.ProfileController;
-import com.example.remindme.service.EventService;
-import com.example.remindme.service.UserEntityService;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.mock.web.MockHttpSession;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
